2da4d8a93c20f2f978799aa9f6786bf076c0b2e0
Will DeBerry <willdeberry@gmail.com>
2017-06-04 12:51:54 -0400

    fix disqus integration

--- conf.py
+++ conf.py
@@ -941,7 +941,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "https-sudoservers-com.disqus.com"
+COMMENT_SYSTEM_ID = "https-sudoservers-com"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
